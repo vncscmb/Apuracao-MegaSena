b689780027400fad9da917efb5b53f55024dbfc4
Vinícius Coelho de Matos Batista <viniciuscmb@dinfor.uniceub>
2026-09-02 08:44:39 -0300

    feat: parametrizacao da quantidade de linhas via terminal

--- gerador.py
+++ gerador.py
@@ -1,18 +1,16 @@
 import random
 import csv
+import sys
 
 def gerar_aposta():
     id_aposta = f"AP-{''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=6))}"
-    qtd_numeros = random.randint(4, 17) # Gera intencionalmente números inválidos
+    qtd_numeros = random.randint(6, 15)
     numeros = random.sample(range(1, 61), qtd_numeros)
     return [id_aposta] + numeros
 
+quantidade = int(sys.argv[1]) if len(sys.argv) > 1 else 5
+
 with open("apostas.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
-    geradas = 0
-    while geradas < 5:
-        aposta = gerar_aposta()
-        # Valida se possui no mínimo 6 e máximo 15 números (descontando o ID)
-        if 6 <= len(aposta) - 1 <= 15:
-            writer.writerow(aposta)
-            geradas += 1
+    for _ in range(quantidade):
+        writer.writerow(gerar_aposta())
