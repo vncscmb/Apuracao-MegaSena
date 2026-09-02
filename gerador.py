bbb0300d6b568223a24affe3358dadb6835896fb
Vinícius Coelho de Matos Batista <viniciuscmb@dinfor.uniceub>
2026-09-02 08:42:05 -0300

    feat: adiciona regra de minimo 6 e maximo 15 numeros

--- gerador.py
+++ gerador.py
@@ -3,10 +3,16 @@ import csv
 
 def gerar_aposta():
     id_aposta = f"AP-{''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=6))}"
-    numeros = random.sample(range(1, 61), 6)
+    qtd_numeros = random.randint(4, 17) # Gera intencionalmente números inválidos
+    numeros = random.sample(range(1, 61), qtd_numeros)
     return [id_aposta] + numeros
 
 with open("apostas.csv", mode="w", newline="") as file:
     writer = csv.writer(file)
-    for _ in range(5):
-        writer.writerow(gerar_aposta())
+    geradas = 0
+    while geradas < 5:
+        aposta = gerar_aposta()
+        # Valida se possui no mínimo 6 e máximo 15 números (descontando o ID)
+        if 6 <= len(aposta) - 1 <= 15:
+            writer.writerow(aposta)
+            geradas += 1
